657f2e979f957c49d8003f3f23cbb881507eb55b
Goshenkata <goshenkataklev@gmail.com>
2022-01-01 16:46:58 +0200

    init

---
+++ src/main/java/com/app/Kmail/KmailApplication.java
@@ -0,0 +1,13 @@
+package com.app.Kmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/Kmail/KmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Kmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
